3c276a8c6c596541a63caa189ffc9705842bc408
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-02 23:54:44 +0530

    ADDED: Employee Attendance

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -2,9 +2,14 @@
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
-		System.out.println(" ====> WelCome To Employee Wage Computation <==== ");
-		
-
+		System.out.println("           ====> WelCome To Employee Wage Computation <====         ");
+        int EmpCheck = 1;
+        double present = Math.floor(Math.random()*10) %2;
+        if (present==EmpCheck)
+        	System.out.println("Employee is present");
+        else
+        	
+        	System.out.println("Employee is abscent");
 	}
 
 }
